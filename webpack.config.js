84f81e196efa80d7506f13914d67af35a2ebbc46
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-03-07 19:12:08 +0900

    :spark:ejsを使えるようにしてみる

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,66 @@
 const path = require('path');
 const outputPath = path.resolve(__dirname, 'dist');
 const webpack = require('webpack');
-module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
-  output: {
-    path: outputPath + '/js',
-    publicPath: '/js', // ホットリロードさせるためにbundle.jsの位置を指定
-    filename: 'bundle.js',
-  },
-  devServer: {
-    contentBase: outputPath,
-    open: true,
-    hot: true,
-    // inline: true,
-    // watchContentBase: true,
-    // port: 8080,
-  },
-  module: {
-    rules: [
-      {
-        // 拡張子 .js の場合
-        test: /\.js$/,
-        use: [
-          {
-            // Babel を利用する
-            loader: 'babel-loader',
-            // exclude: /node_modules/,
-            // Babel のオプションを指定する
-            options: {
-              presets: [
-                [
-                  // プリセットを指定することで、ES2020 を ES5 に変換
-                  '@babel/preset-env',
-                  {
-                    useBuiltIns: 'entry',
-                    corejs: { version: 3, proposals: false },
-                  },
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const paths = {
+  dist: './dist/',
+  src: './src/',
+};
+
+module.exports = [
+  {
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+      path: outputPath + '/js',
+      publicPath: '/js', // ホットリロードさせるためにbundle.jsの位置を指定
+      filename: 'bundle.js',
+    },
+    devServer: {
+      contentBase: outputPath,
+      open: true,
+      hot: true,
+      // inline: true,
+      watchContentBase: true,
+      // port: 8080,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ejs$/i,
+          // include: `src/ejs/`,
+          use: ['ejs-easy-loader', 'html-loader'],
+        },
+        {
+          // 拡張子 .js の場合
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              // exclude: /node_modules/,
+              options: {
+                presets: [
+                  [
+                    // プリセットを指定することで、ES2020 を ES5 に変換
+                    '@babel/preset-env',
+                    {
+                      useBuiltIns: 'entry',
+                      corejs: { version: 3, proposals: false },
+                    },
+                  ],
                 ],
-              ],
-              // plugins: ['@babel/plugin-transform-runtime'],
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: '../top.html', // bundle.jsからの相対位置
+        template: 'src/ejs/index.ejs',
+        // alwaysWriteToDisk: true,
+      }),
     ],
   },
-  // plugins: [
-  //   new webpack.HotModuleReplacementPlugin(),
-  // ],
-  // watch: true,
-  // watchOptions: {
-  //   ignored: /node_modules/  //正規表現で指定（node_modules を除外）
-  // }
-};
+];
